b6e7401dcd3fb0264c727b74ccc926ce0715800d
gabrieltoadere <gabriel.toadere.2024@mumail.ie>
2025-09-30 11:32:41 +0100

    added links to pages

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ Array.from(document.getElementsByClassName("menu-item"))
     item.onmouseover = () => {
       menu.dataset.activeIndex = index;
     }
-  });
+});
+
+
+document.getElementById("cards").onmousemove = e => {
+for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
